b683da21d26f40b6aa0df06ac68253f7f0f7eb9c
Melody Cartwright <melody94c@gmail.com>
2025-03-27 11:04:27 +0100

    removed userid requirement from CarBid.js

--- backend/src/models/CarBid.js
+++ backend/src/models/CarBid.js
@@ -1,22 +1,14 @@
 const mongoose = require("mongoose");
 
 const carBidSchema = new mongoose.Schema({
-  amount: {
-    type: Number,
-    required: true,
-  },
+  amount: { type: Number, required: true },
   carAuctionId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "CarAuction", // Reference to CarAuction model
+    ref: "CarAuction",
     required: true,
   },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User", // Reference to User model
-    required: true,
-  },
+  // Removed userId field as it's not needed
 });
 
 const CarBid = mongoose.model("CarBid", carBidSchema);
-
 module.exports = CarBid;
