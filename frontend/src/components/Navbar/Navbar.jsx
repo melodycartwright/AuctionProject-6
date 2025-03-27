ba08214a92ac0f561eb8de54c94c2ea7b61169c8
gul <gulmiracu@gmail.com>
2025-03-27 02:19:03 +0100

    add tailwind and react-hook-form

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,12 +2,12 @@ import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-        <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/login">Sign in</NavLink></li>
-            <li><NavLink to="/register">Sign up</NavLink></li>
-        </ul>
+    <nav>
+      <ul className="flex gap-2 bg-purple-200 p-2">
+        <li className="hover:underline"><NavLink to="/">Home</NavLink></li>
+        <li className="hover:underline"><NavLink to="/login">Sign in</NavLink></li>
+        <li className="hover:underline"><NavLink to="/register">Sign up</NavLink></li>
+      </ul>
     </nav>
   )
 }
